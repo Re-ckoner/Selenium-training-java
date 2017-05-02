e408d60f735b72a8f0bd0c41e4a4e85f22ab066f
DESKTOP-5J639TU\Andy <only1reckoner@gmail.com>
2017-05-03 01:55:46 +0300

    final vercion of 7 task

--- src/test/java/ru/stqa/training/selenium/LoginAdmin.java
+++ src/test/java/ru/stqa/training/selenium/LoginAdmin.java
@@ -2,6 +2,7 @@ package ru.stqa.training.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
@@ -41,8 +42,8 @@ public class LoginAdmin extends TestBase {
     {
 
 
-        //driver.get("http://localhost/litecart/admin");
-        driver.get("http://localhost/lightcart/public_html/admin");
+        driver.get("http://localhost/litecart/admin");
+       // driver.get("http://localhost/lightcart/public_html/admin");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.cssSelector("button.btn.btn-default")).click();
@@ -50,41 +51,49 @@ public class LoginAdmin extends TestBase {
         System.out.println("number of main menu entries " + driver.findElements(By.cssSelector("#box-apps-menu-wrapper li")).size());
         System.out.println("number of main menu entries " + driver.findElements(By.cssSelector("span.name")).size()); //just another way
 
-        WebElement e; //for storing the sub-menu
-
         driver.findElement(By.linkText("Appearance")).click();
+        assert (isHeaderPresent(driver));
+
         driver.findElement(By.linkText("Template")).click();
+        assert (isHeaderPresent(driver));
+
         driver.findElement(By.linkText("Logotype")).click();
+        assert (isHeaderPresent(driver));
 
         driver.findElement(By.linkText("Catalog")).click();
-        e =  driver.findElement(By.cssSelector("ul.docs"));
-             e.findElement(By.linkText("Product Groups")).click();
-             e.findElement(By.linkText("Option Groups")).click();
-             e.findElement(By.linkText("Catalog")).click();
-             e.findElement(By.linkText("Manufacturers")).click();
-             e.findElement(By.linkText("Suppliers")).click();
-             e.findElement(By.linkText("Delivery Statuses")).click();
-             e.findElement(By.linkText("Sold Out Statuses")).click();
-             e.findElement(By.linkText("Quantity Units")).click();
-             e.findElement(By.linkText("CSV Import/Export")).click();
+        assert (isHeaderPresent(driver));
+             driver.findElement(By.linkText("Product Groups")).click();
+             driver.findElement(By.linkText("Option Groups")).click();
+             driver.findElement(By.linkText("Manufacturers")).click();
+             driver.findElement(By.linkText("Suppliers")).click();
+             driver.findElement(By.linkText("Delivery Statuses")).click();
+             driver.findElement(By.linkText("Sold Out Statuses")).click();
+             driver.findElement(By.linkText("Quantity Units")).click();
+             driver.findElement(By.linkText("CSV Import/Export")).click();
+             driver.findElement(By.id("doc-catalog")).click();
 
         driver.findElement(By.linkText("Countries")).click();
+        assert (isHeaderPresent(driver));
+
         driver.findElement(By.linkText("Currencies")).click();
+        assert (isHeaderPresent(driver));
 
         driver.findElement(By.linkText("Customers")).click();
-        e =  driver.findElement(By.cssSelector(".docs"));
-            e.findElement(By.linkText("CSV Import/Export")).click();
-            e.findElement(By.linkText("Newsletter")).click();
-            e.findElement(By.linkText("Customers")).click();
+        assert (isHeaderPresent(driver));
+             driver.findElement(By.linkText("CSV Import/Export")).click();
+             driver.findElement(By.linkText("Newsletter")).click();
+             driver.findElement(By.cssSelector("#doc-customers")).click();
 
         driver.findElement(By.linkText("Geo Zones")).click();
+        assert (isHeaderPresent(driver));
 
         driver.findElement(By.linkText("Languages")).click();
-        e =  driver.findElement(By.cssSelector("#doc-catalog"));
-         e.findElement(By.linkText("Storage Encoding")).click();
-         e.findElement(By.linkText("Languages")).click();
+        assert (isHeaderPresent(driver));
+             driver.findElement(By.linkText("Storage Encoding")).click();
+             driver.findElement(By.cssSelector("#doc-languages")).click();
 
         driver.findElement(By.linkText("Modules")).click();
+        assert (isHeaderPresent(driver));
              driver.findElement(By.linkText("Customer Modules")).click();
              driver.findElement(By.linkText("Shipping Modules")).click();
              driver.findElement(By.linkText("Payment Modules")).click();
@@ -93,22 +102,51 @@ public class LoginAdmin extends TestBase {
              driver.findElement(By.linkText("Job Modules")).click();
 
         driver.findElement(By.linkText("Orders")).click();
-        e =  driver.findElement(By.cssSelector("#doc-catalog"));
-            e.findElement(By.linkText("Order Statuses")).click();
-            e.findElement(By.linkText("Orders")).click();
+        assert (isHeaderPresent(driver));
+            driver.findElement(By.linkText("Order Statuses")).click();
+            driver.findElement(By.cssSelector("#doc-orders")).click();
 
         driver.findElement(By.linkText("Pages")).click();
-        e =  driver.findElement(By.cssSelector("#doc-catalog"));
-            e.findElement(By.linkText("CSV Import/Export")).click();
-            e.findElement(By.linkText("Pages")).click();
+        assert (isHeaderPresent(driver));
+            driver.findElement(By.linkText("CSV Import/Export")).click();
+            driver.findElement(By.cssSelector("#doc-pages")).click();
 
         driver.findElement(By.linkText("Reports")).click();
+        assert (isHeaderPresent(driver));
+            driver.findElement(By.linkText("Monthly Sales")).click();
+            driver.findElement(By.linkText("Most Sold Products")).click();
+            driver.findElement(By.linkText("Most Shopping Customers")).click();
+
         driver.findElement(By.linkText("Settings")).click();
+        assert (isHeaderPresent(driver));
+           driver.findElement(By.linkText("Store Info")).click();
+           driver.findElement(By.linkText("Defaults")).click();
+           driver.findElement(By.linkText("General")).click();
+           driver.findElement(By.linkText("Listings")).click();
+           driver.findElement(By.linkText("Images")).click();
+           driver.findElement(By.linkText("Checkout")).click();
+           driver.findElement(By.linkText("Advanced")).click();
+           driver.findElement(By.linkText("Security")).click();
+
         driver.findElement(By.linkText("Slides")).click();
+        assert (isHeaderPresent(driver));
+
         driver.findElement(By.linkText("Tax")).click();
+        assert (isHeaderPresent(driver));
+            driver.findElement(By.linkText("Tax Rates")).click();
+            driver.findElement(By.linkText("Tax Classes")).click();
+
         driver.findElement(By.linkText("Translations")).click();
+        assert (isHeaderPresent(driver));
+            driver.findElement(By.linkText("Search Translations")).click();
+            driver.findElement(By.linkText("Scan Files")).click();
+            driver.findElement(By.linkText("CSV Import/Export")).click();
+
         driver.findElement(By.linkText("Users")).click();
+        assert (isHeaderPresent(driver));
+
         driver.findElement(By.linkText("vQmods")).click();
+        assert (isHeaderPresent(driver));
     }
 
         public void printList(List<WebElement> list){
@@ -116,6 +154,12 @@ public class LoginAdmin extends TestBase {
             System.out.println("sub-menu "+ s.getText());
         }
             System.out.println("==================================================================");
+       }
+
+       public boolean isHeaderPresent(WebDriver d)
+       {
+           List<WebElement> list= d.findElements(By.cssSelector("h1"));
+           return list.size()>0;
        }
 
 
