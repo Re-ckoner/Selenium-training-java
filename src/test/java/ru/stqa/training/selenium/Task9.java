8438ed2f2de56c429a8e0874b1055290ed49c173
DESKTOP-5J639TU\Andy <only1reckoner@gmail.com>
2017-05-06 16:37:45 +0300

    9th ready

--- src/test/java/ru/stqa/training/selenium/Task9.java
+++ src/test/java/ru/stqa/training/selenium/Task9.java
@@ -14,25 +14,30 @@ public class Task9 extends TestBase
     @Test
     public void testCountries() throws InterruptedException
     {
-       // driver.get("http://localhost/litecart/public_html/admin/?app=countries&doc=countries");
-        driver.get("http://localhost/lightcart/public_html/admin//?app=countries&doc=countries");
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+       // driver.get("http://localhost/lightcart/public_html/admin//?app=countries&doc=countries");
         logIn();
-        driver.get("http://localhost/lightcart/public_html/admin//?app=countries&doc=countries");
-        List<WebElement> countries = driver.findElements(By.cssSelector(".table.table-striped.data-table tbody tr"));
+        driver.get("http://localhost/litecart/admin//?app=countries&doc=countries");
+        List<WebElement> countries = driver.findElements(By.cssSelector(".table.table-striped.data-table tbody tr a:not([title=Edit])"));
         System.out.println("countries found "+countries.size());
         System.out.println("last "+countries.get(countries.size()-1).getText());
-        System.out.println("first "+countries.get(0).getText());
-
-        Thread.sleep(6000);
-
+        System.out.println("first "+countries.get(0).getAttribute("textContent"));
+        assert(isSorted(countries));
+    //    isSorted(countries);
     }
 
     boolean isSorted(List<WebElement> list){
-        String[] arr = new String[list.size()];
 
-        for (WebElement e:list){
-            arr
+        for (int i=1; i<list.size()-1; i++){
+            //arr[i] = list.get(i).getText();
+           // System.out.println(list.get(i).getText() +" "+ list.get(i-1).getText()+ " compare result= "  + list.get(i).getText().compareTo(list.get(i-1).getText()));
+            if(list.get(i).getText().compareTo(list.get(i-1).getText())<0){
+                System.out.println(list.get(i).getText() +" "+ list.get(i-1).getText()+ " compare result= "  + list.get(i).getText().compareTo(list.get(i-1).getText()));
+                return false;
+            }
         }
-    }
+        return true;
+
+   }
 
 }
