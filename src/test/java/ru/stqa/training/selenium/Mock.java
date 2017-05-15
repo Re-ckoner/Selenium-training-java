b1e13f92aefc178cdeed434067a030fddb420eb0
DESKTOP-5J639TU\Andy <only1reckoner@gmail.com>
2017-05-16 01:14:28 +0300

    9b finished

---
+++ src/test/java/ru/stqa/training/selenium/Mock.java
@@ -0,0 +1,7 @@
+package ru.stqa.training.selenium;
+
+/**
+ * Created by Andy on 16.05.2017.
+ */
+public class Mock {
+}
--- src/test/java/ru/stqa/training/selenium/Task9b.java
+++ src/test/java/ru/stqa/training/selenium/Task9b.java
@@ -10,40 +10,48 @@ import java.util.List;
 /**
  * Created by amil on 5/5/2017.
  */
-public class Task9b extends TestBase{
+public class Task9b extends TestBase {
     @Test
-    public void testCountries1() throws InterruptedException
-    {
-        //driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
-        driver.get("http://localhost/lightcart/public_html/admin//?app=countries&doc=countries");
+    public void testCountries1() throws InterruptedException {
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+//        driver.get("http://localhost/lightcart/public_html/admin//?app=countries&doc=countries");
         logIn();
         // driver.get("http://localhost/litecart/admin//?app=countries&doc=countries");
         List<WebElement> lines = driver.findElements(By.cssSelector(
                 ".table.table-striped.data-table tbody tr"));
-        System.out.println("lines found "+lines.size());
-        List<Integer>lineNums =new ArrayList<Integer>();
-        for (int i=0; i<lines.size(); i++){
-            if(!(lines.get(i).findElement(By.cssSelector("td.text-center")).getText().equals("0"))){
+        List<String> links = new ArrayList<String>();
+        System.out.println("lines found " + lines.size());
+        List<Integer> lineNums = new ArrayList<Integer>();
+        for (int i = 0; i < lines.size(); i++) {
+            if (!(lines.get(i).findElement(By.cssSelector("td.text-center")).getText().equals("0"))) {
                 lineNums.add(i);
             }
         }
 
-        for (Integer i:lineNums)
-        {
-            System.out.println(i);
+        for (Integer i : lineNums) {
+            links.add(lines.get(i).findElement(By.cssSelector("a:not([title=Edit])")).getAttribute("href"));
+        }
+        for (String l : links) {
+          //  System.out.println(l);
+            driver.get(l);
+            //logIn();
+            List<WebElement> geoZones = driver.findElements(By.cssSelector("tbody>tr>td>input[name*=name]"));
+            assert (isSortedb(geoZones));
+           // System.out.println(isSortedb(geoZones));
+
         }
 
 
     }
 
 
-    boolean isSorted(List<WebElement> list){
+    boolean isSortedb(List<WebElement> list) {
 
-        for (int i=1; i<list.size()-1; i++){
+        for (int i = 1; i < list.size() - 1; i++) {
             //arr[i] = list.get(i).getText();
             // System.out.println(list.get(i).getText() +" "+ list.get(i-1).getText()+ " compare result= "  + list.get(i).getText().compareTo(list.get(i-1).getText()));
-            if(list.get(i).getText().compareTo(list.get(i-1).getText())<0){
-                System.out.println(list.get(i).getText() +" "+ list.get(i-1).getText()+ " compare result= "  + list.get(i).getText().compareTo(list.get(i-1).getText()));
+            if (list.get(i).getAttribute("value").compareTo(list.get(i - 1).getAttribute("value")) < 0) {
+                System.out.println(list.get(i).getAttribute("value") + " " + list.get(i - 1).getAttribute("value") + " compare result= " + list.get(i).getAttribute("value").compareTo(list.get(i - 1).getAttribute("value")));
                 return false;
             }
         }
