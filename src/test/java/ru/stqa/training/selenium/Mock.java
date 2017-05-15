58c0f904b111fed479b2855b0e3486255c4f6172
DESKTOP-5J639TU\Andy <only1reckoner@gmail.com>
2017-05-16 01:22:48 +0300

    9 full

--- src/test/java/ru/stqa/training/selenium/Mock.java
+++ src/test/java/ru/stqa/training/selenium/Mock.java
@@ -1,7 +1,24 @@
 package ru.stqa.training.selenium;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 /**
  * Created by Andy on 16.05.2017.
  */
-public class Mock {
+public class Mock extends TestBase {
+    @Test
+    public void justMock(){
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=edit_country&country_code=CA");
+        logIn();
+        List<WebElement> geoZones = driver.findElements(By.cssSelector("tbody>tr>td>input[name*=name]"));
+        System.out.println(geoZones.size());
+        for (WebElement e:geoZones) {
+            System.out.println(e.getAttribute("value"));
+        }
+    }
+
 }
--- src/test/java/ru/stqa/training/selenium/Task9.java
+++ src/test/java/ru/stqa/training/selenium/Task9.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,31 +14,17 @@ public class Task9 extends TestBase
     @Test
     public void testCountries() throws InterruptedException
     {
-        //driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
-        driver.get("http://localhost/lightcart/public_html/admin//?app=countries&doc=countries");
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+       // driver.get("http://localhost/lightcart/public_html/admin//?app=countries&doc=countries");
         logIn();
-      // driver.get("http://localhost/litecart/admin//?app=countries&doc=countries");
+        driver.get("http://localhost/litecart/admin//?app=countries&doc=countries");
         List<WebElement> countries = driver.findElements(By.cssSelector(".table.table-striped.data-table tbody tr a:not([title=Edit])"));
         System.out.println("countries found "+countries.size());
         System.out.println("last "+countries.get(countries.size()-1).getText());
         System.out.println("first "+countries.get(0).getAttribute("textContent"));
-      //  assert(isSorted(countries));
+        assert(isSorted(countries));
     //    isSorted(countries);
-
-        List<WebElement> zones = driver.findElements(By.cssSelector(".table.table-striped.data-table > tbody > tr > td.text-center"));
-        System.out.println("zones found "+ zones.size());
-        List<String> notEmptyZones = new ArrayList<String>();
-        for (WebElement e:zones){
-            if(!(e.getText().equals("0"))){
-            notEmptyZones.add(e.getText());
-            }
-        }
-
-        for(int i=0; i<countries.size()-1; i++){
-            countries.get(i);
-        }
     }
-
 
     boolean isSorted(List<WebElement> list){
 
