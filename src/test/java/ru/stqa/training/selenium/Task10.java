f46ddfbf97c589a92a2ffeba351e32fc5ae9d961
DESKTOP-5J639TU\Andy <only1reckoner@gmail.com>
2017-05-17 00:25:51 +0300

    works on chrome

--- src/test/java/ru/stqa/training/selenium/Task10.java
+++ src/test/java/ru/stqa/training/selenium/Task10.java
@@ -12,7 +12,8 @@ public class Task10 extends TestBase{
 
     @Test
     public void testGoods() throws Exception{
-        driver.get("http://localhost/lightcart/public_html");
+       // driver.get("http://localhost/lightcart/public_html");
+        driver.get("http://localhost/litecart");
 
         HashMap<String, String> map = new HashMap<String, String>();
         map.put("name", driver.findElement(
@@ -24,6 +25,7 @@ public class Task10 extends TestBase{
         //System.out.println(map.get("name"));
         String grey = driver.findElement(By.cssSelector("#box-campaigns > div > div > div > a .price-wrapper s")).getCssValue("color");
         String red = driver.findElement(By.cssSelector("#box-campaigns > div > div > div > a .price-wrapper strong")).getCssValue("color");
+        System.out.print(grey);
         assert((parseColor(grey)[0].equals(parseColor(grey)[1]))&&parseColor(grey)[0].equals(parseColor(grey)[2]));
         assert((parseColor(red)[1].equals("0"))&&(parseColor(red)[2].equals("0")));
 
@@ -31,8 +33,9 @@ public class Task10 extends TestBase{
         assert(font.contains("line-through"));
         String fontA = driver.findElement(By.cssSelector("#box-campaigns > div > div > div > a .price-wrapper strong")).getCssValue("font-weight");
         assert(fontA.contains("bold"));
-
-
+        String fontSize = driver.findElement(By.cssSelector("#box-campaigns > div > div > div > a .price-wrapper s")).getCssValue("font-size");
+        String fontSizeA = driver.findElement(By.cssSelector("#box-campaigns > div > div > div > a .price-wrapper strong")).getCssValue("font-size");
+        assert(fontSizeA.compareTo(fontSize)>0);
         // assert (parseColor(grey)[1]==parseColor(grey)[2]);
 
 
@@ -43,6 +46,19 @@ public class Task10 extends TestBase{
         //System.out.println(driver.findElement(By.cssSelector("#box-product > div.row > div:nth-child(2) > h1")).getText());
         assert(driver.findElement(By.cssSelector(".regular-price")).getText().equals(map.get("price")));
         assert(driver.findElement(By.cssSelector(".campaign-price")).getText().equals(map.get("priceA")));
+        grey = driver.findElement(By.cssSelector(".regular-price")).getCssValue("color");
+        red = driver.findElement(By.cssSelector(".campaign-price")).getCssValue("color");
+        assert((parseColor(grey)[0].equals(parseColor(grey)[1]))&&parseColor(grey)[0].equals(parseColor(grey)[2]));
+        assert((parseColor(red)[1].equals("0"))&&(parseColor(red)[2].equals("0")));
+
+        font = driver.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration");
+        assert(font.contains("line-through"));
+        fontA = driver.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight");
+        assert(fontA.contains("bold"));
+
+        fontSize = driver.findElement(By.cssSelector(".regular-price")).getCssValue("font-size");
+        fontSizeA = driver.findElement(By.cssSelector(".campaign-price")).getCssValue("font-size");
+        assert(fontSizeA.compareTo(fontSize)>0);
     }
 
 
