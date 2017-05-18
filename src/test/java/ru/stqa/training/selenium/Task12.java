12b37a23268546692c2df506a9f89f07b1b19730
DESKTOP-5J639TU\Andy <only1reckoner@gmail.com>
2017-05-18 23:20:22 +0300

    12 ready

--- src/test/java/ru/stqa/training/selenium/Task12.java
+++ src/test/java/ru/stqa/training/selenium/Task12.java
@@ -2,6 +2,7 @@ package ru.stqa.training.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
@@ -16,13 +17,14 @@ public class Task12 extends TestBase{
     @Test
     public void addGoods() throws InterruptedException, IOException
     {
-        driver.get("http://localhost/lightcart/public_html/admin");
-        //driver.get("http://localhost/litecart/admin");
+       // driver.get("http://localhost/lightcart/public_html/admin");
+        driver.get("http://localhost/litecart/admin");
         logIn();
         driver.findElement(By.linkText("Catalog")).click();
         driver.findElement(By.cssSelector("#main > ul > li:nth-child(3) > a")).click();
         driver.findElement(By.cssSelector(".form-group .form-control[name=code]")).sendKeys("12345");
         driver.findElement(By.cssSelector(".form-group .form-control[name*=name]")).sendKeys("Suicide duck");
+        driver.findElement(By.cssSelector("#tab-general > div > div:nth-child(1) > div:nth-child(1) > div > div > label:nth-child(1)")).click();
         driver.findElement(By.cssSelector(".form-group .form-control[name=sku]")).sendKeys("sku");
         driver.findElement(By.cssSelector(".form-group .form-control[name=gtin]")).sendKeys("gtin");
         driver.findElement(By.cssSelector(".form-group .form-control[name=taric]")).sendKeys("taric");
@@ -44,14 +46,30 @@ public class Task12 extends TestBase{
 
         //info tab
         driver.findElement(By.cssSelector("#main form div ul li a[href*=info]")).click();
+        //driver.findElement(By.cssSelector("#main form div ul li:nth-child(2) a")).click();
+        driver.findElement(By.cssSelector("#main form div ul li a[href*=info]")).click();
+        Select manuf = new Select(driver.findElement(By.cssSelector("#tab-information select[name=manufacturer_id]")));
+        manuf.selectByIndex(1);
+        // Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#tab-information > div:nth-child(2) > div > input")).sendKeys("duck, fun, bath, electricity");
+        driver.findElement(By.cssSelector("#tab-information > div:nth-child(3) > div > div > input")).sendKeys("bath like hell!"+ Keys.TAB+
+                "Bath with fun"+Keys.TAB +Keys.TAB +"Attributes" + Keys.TAB+"title" +Keys.TAB +"meta");
 
-
+        //prices
+        driver.findElement(By.cssSelector("#main form div ul li a[href*=prices]")).click();
+        driver.findElement(By.cssSelector("#prices > div > div:nth-child(1) > div > input")).clear();
+        driver.findElement(By.cssSelector("#prices > div > div:nth-child(1) > div > input")).sendKeys("10"+Keys.TAB+Keys.DOWN);
+        driver.findElement(By.cssSelector("#prices > table > tbody > tr:nth-child(1) > td:nth-child(2) > input")).clear();
+        driver.findElement(By.cssSelector("#prices > table > tbody > tr:nth-child(2) > td:nth-child(2) > input")).clear();
+        driver.findElement(By.cssSelector("#prices > table > tbody > tr:nth-child(1) > td:nth-child(2) > input")).sendKeys("10");
+        driver.findElement(By.cssSelector("#prices > table > tbody > tr:nth-child(2) > td:nth-child(2) > input")).sendKeys("9");
 
         driver.findElement(By.cssSelector("#main > form > p > button:nth-child(1)")).click();
 
+        driver.findElement(By.linkText("Suicide duck"));
 
 
-        Thread.sleep(5000);
+      
     }
 
 
