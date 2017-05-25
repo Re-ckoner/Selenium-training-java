c2b64e75ed81ed3bffe4d41ddcff5b03f8b6b989
Andy <only1reckoner@gmail.com>
2017-05-25 22:04:10 +0300

    Task 13a added

--- src/test/java/ru/stqa/training/selenium/Task13a.java
+++ src/test/java/ru/stqa/training/selenium/Task13a.java
@@ -16,7 +16,8 @@ public class Task13a extends TestBase{
     int goodsNumber;
 
     public void open(){
-        driver.get("http://localhost/litecart/en/");
+       // driver.get("http://localhost/litecart/en/");
+        driver.get("http://localhost/lightcart/public_html");
     }
 
     public int getGoodsNum()
@@ -25,13 +26,16 @@ public class Task13a extends TestBase{
     }
 
     public void addDuck(){
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.ESCAPE);
         driver.findElement(By.cssSelector("#main #content a.link")).click();
-        Select select = new Select(driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div.form-group.required > select")));
-        select.selectByIndex(2);
+        Select select1 = new Select(driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div > select.form-control")));
+        select1.selectByIndex(3);
         driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div:nth-child(4) > div > div:nth-child(2) > button")).click();
+        driver.findElement(By.cssSelector("#box-product")).click();
     }
 
     public boolean checkCart(int gn){
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.ESCAPE);
         try{
         wait.until((WebDriver d) -> d.findElement(By.cssSelector("#cart > a > div > div:nth-child(2) > span.quantity")).getText().equals(Integer.toString(gn)));
         }
@@ -41,54 +45,71 @@ public class Task13a extends TestBase{
         return true;
     }
 
+    public void emptyTrashCan(){
+
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.ESCAPE);
+        driver.findElement(By.id("cart")).click();
+        int counter = driver.findElements(By.cssSelector("#box-checkout-cart > div > table > tbody tr")).size();
+        while (driver.findElements(By.cssSelector("#box-checkout-cart > div > table > tbody tr")).size()>0)
+        {
+            driver.findElement(By.cssSelector("#box-checkout-cart div.table-responsive tbody tr td:last-child button")).click();
+            wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("#box-checkout-cart > div > table > tbody tr"), counter - 1));
+            counter--;
+        }
+
+    }
+
     @Test
-    public void trashCan() throws InterruptedException
-    {
+    public void trashCan() throws InterruptedException{
         //driver.get("http://localhost/lightcart/public_html/en/");
 
         open();
         goodsNumber = getGoodsNum();
         addDuck();
         goodsNumber++;
-        checkCart(goodsNumber);
+        assert(checkCart(goodsNumber));
+        open();
         addDuck();
+        goodsNumber++;
+        assert(checkCart(goodsNumber));
+        emptyTrashCan();
         //check cart
 
 
 
         //add 2
-        driver.get("http://localhost/litecart/en/");
+      //  driver.get("http://localhost/litecart/en/");
         //goodsNum = Integer.parseInt(driver.findElement(By.cssSelector("#cart > a > div > div:nth-child(2) > span.quantity")).getText());
-        driver.findElement(By.cssSelector("#main #content a.link")).click();
-        driver.findElement(By.cssSelector("#box-product"));
-
-        Select select1 = new Select(driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div > select.form-control")));
-        select1.selectByIndex(3);
-        driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div:nth-child(4) > div > div:nth-child(2) > button")).click();
-        driver.findElement(By.cssSelector("#box-product")).click();
+//        driver.findElement(By.cssSelector("#main #content a.link")).click();
+//        driver.findElement(By.cssSelector("#box-product"));
+//
+//        Select select1 = new Select(driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div > select.form-control")));
+//        select1.selectByIndex(3);
+//        driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div:nth-child(4) > div > div:nth-child(2) > button")).click();
+//        driver.findElement(By.cssSelector("#box-product")).click();
         //goodsNum++;
        // wait.until((WebDriver d) -> d.findElement(By.cssSelector("#cart > a > div > div:nth-child(2) > span.quantity")).getText().equals(Integer.toString(goodsNum)));
 
         //add 3
-        driver.get("http://localhost/litecart/en/");
+      //  driver.get("http://localhost/litecart/en/");
         //goodsNum = Integer.parseInt(driver.findElement(By.cssSelector("#cart > a > div > div:nth-child(2) > span.quantity")).getText());
-        driver.findElement(By.cssSelector("#main #content a.link")).click();
+      //  driver.findElement(By.cssSelector("#main #content a.link")).click();
         //select = new Select(driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div > select.form-control")));
         //select.selectByIndex(1);
-        driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div:nth-child(4) > div > div:nth-child(2) > button")).click();
+       // driver.findElement(By.cssSelector("#box-product > div > div:nth-child(3) > div > div.buy_now > form > div:nth-child(4) > div > div:nth-child(2) > button")).click();
         //goodsNum++;
        // wait.until((WebDriver d) -> d.findElement(By.cssSelector("#cart > a > div > div:nth-child(2) > span.quantity")).getText().equals(Integer.toString(goodsNum)));
 
         //remove
-        driver.findElement(By.cssSelector("body")).sendKeys(Keys.ESCAPE);
-        driver.findElement(By.id("cart")).click();
-        while (driver.findElements(By.cssSelector("#box-checkout-cart > div > table > tbody tr")).size()>0){
-            driver.findElement(By.cssSelector("#box-checkout-cart div.table-responsive tbody tr td:last-child button")).click();
-          //  wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("#box-checkout-cart > div > table > tbody tr"),goodsNum-1));
-            //goodsNum--;
+//        driver.findElement(By.cssSelector("body")).sendKeys(Keys.ESCAPE);
+//        driver.findElement(By.id("cart")).click();
+//        while (driver.findElements(By.cssSelector("#box-checkout-cart > div > table > tbody tr")).size()>0){
+//            driver.findElement(By.cssSelector("#box-checkout-cart div.table-responsive tbody tr td:last-child button")).click();
+//          //  wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("#box-checkout-cart > div > table > tbody tr"),goodsNum-1));
+//            //goodsNum--;
         }
 
 
 
     }
-}
+
